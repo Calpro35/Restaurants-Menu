f1a199afe990741da133769d03f324e5d64fa01f
Carlos <calpro35@outlook.com>
2024-12-07 12:31:07 -0300

    update

---
+++ src/main/java/com/react/MenuList/MenuListApplication.java
@@ -0,0 +1,13 @@
+package com.react.MenuList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuListApplication.class, args);
+	}
+
+}
